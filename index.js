d8f4631182d0cd69cdc548a8e62cdab984b67967
Eliezir Moreira <eliezir2004@gmail.com>
2022-06-01 11:14:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -126,9 +126,9 @@ $(document).on("keypress", function () {
     }
 })
 
-$(document).on("click", function () {
+/*$(document).on("click", function () {
     if (gameOver === true) {
         nextColor();
         gameOver = false;
     }
-})
+})*/
